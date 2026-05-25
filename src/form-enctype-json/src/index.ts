f43b98c1ac843e42e9021a5c3435b99e72ea08b7
Sérgio Carvalho <sergio.carvalho@sooma.com>
2026-05-25 14:46:59 +0100

    Implement form-enctype-json form submission

--- src/form-enctype-json/src/index.ts
+++ src/form-enctype-json/src/index.ts
@@ -2,9 +2,97 @@ import "../../sergiosgc/src/index";
 import "../../mutation-event-attacher/src/index";
 
 (function() {
+    const formDataToJsonType = function(form: HTMLFormElement, value: FormDataEntryValue, key: string) {
+        let jsonType = "string";
+        const formItem = form.elements.namedItem(key);
+        if (formItem instanceof Element) {
+            const sergiosgcEnc = formItem.getAttribute('sergiosgc-enc');
+            if (sergiosgcEnc) jsonType = sergiosgcEnc;
+        } else if (formItem instanceof NodeList) {
+            const sergiosgcEnc = Array.from(formItem).map(item => item.getAttribute('sergiosgc-enc')).find(enc => enc !== null);
+            if (sergiosgcEnc) jsonType = sergiosgcEnc;
+        }
+        const isArray = jsonType.endsWith('[]');
+        if (isArray) {
+            const parts = value.toString().split(',');
+            return parts.map(part => {
+                switch (jsonType.slice(0, -2)) {
+                    case "boolean": return part === "true";
+                    case "integer": return parseInt(part as string);
+                    case "float": return parseFloat(part as string);
+                    default: return part as string;
+                }
+            });
+        } else {
+            if (jsonType !== "string" && value == "") {
+                return null;
+            }
+            switch (jsonType) {
+                case "boolean": return value === "true";
+                case "integer": return parseInt(value as string);
+                case "float": return parseFloat(value as string);
+                default: return value as string;
+            }
+        }
+    }
     const convertFormToJson = function(ev: SubmitEvent) {
-        const formData = new FormData(ev.target as HTMLFormElement);
-        console.log(formData.get('groups'));
+        const form = ev.target as HTMLFormElement;
+        const formData = new FormData(form);
+        const json: any = {};
+        for (const [key, value] of formData.entries()) {
+            if (key.endsWith('[]') && key.slice(0, -2).indexOf('[]') !== -1) {
+                throw new Error(`Unable to insert key ${key}, because it contains multiple nested arrays`);
+            }
+            if (key === "") {
+                throw new Error(`Unable to insert key ${key}, because it is empty`);
+            }
+            let cursor = json;
+            let partialKey = '';
+            let keyParts = key.split('.');
+            let lastKeyPart = keyParts.pop();
+            if (!lastKeyPart) {
+                throw new Error(`Unable to insert key ${key}, because it is empty`); // Actually impossible because of the empty string check above
+            }
+            for (let part of keyParts) {
+                partialKey += (partialKey ? '.' : '') + part;
+                if (!cursor[part]) {
+                    cursor[part] = {};
+                } else if (typeof cursor[part] !== 'object') {
+                    throw new Error(`Unable to insert key ${partialKey}, because current entry is not a dictionary: ${cursor[part]}`);
+                }
+                cursor = cursor[part];
+            }
+            cursor[lastKeyPart] = formDataToJsonType(form, value, key);
+        }
+
+        const url = form.getAttribute('action') ?? (window.location.href as string);
+        const method = form.getAttribute('method') ?? 'POST';
+        console.log(url, method);
+        fetch(url, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(json),
+            redirect: "manual"
+        })
+        .then(async (response) => {
+            if (response.type === "opaqueredirect" || (response.status >= 300 && response.status < 400 && response.headers.has('Location'))) {
+                const location = response.headers.get("Location") ?? url;
+                (window.location as any) = location;
+                return;
+            }
+            try {
+                const responseData = await response.json();
+                form.dispatchEvent(new CustomEvent('form-enctype-json-response', { bubbles: true, detail: responseData }));
+            } catch (_) {
+                console.error(`HTTP error: ${response.status} ${response.statusText}`);
+            }
+        })
+        .catch(err => {
+            console.error('Failed to submit JSON form:', err);
+        });
         ev.preventDefault();
         ev.stopPropagation();
     };
@@ -15,6 +103,5 @@ import "../../mutation-event-attacher/src/index";
         'submit',
         convertFormToJson as any
     );
-    console.log('form-enctype-json loaded');
 
 })();
